9bd1de4aa6901cc81eeec9188f8c79c2a0c2992a
RajGupta2112 <100618188+RajGupta2112@users.noreply.github.com>
2022-05-25 11:00:24 +0530

    Delete 1st.py

--- 1st.py
+++
@@ -1,7 +0,0 @@
-echo "# RajGupta_S_43_Python_Assignment" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/RajGupta2112/RajGupta_S_43_Python_Assignment.git
-git push -u origin main
